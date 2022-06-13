6f49bbf2c9ae322cc84e2d80c25a322ad4b5b98c
Cleverson Nahum <cleversonahum@gmail.com>
2022-06-13 10:51:40 -0300

    organizing comments

--- comm_env.py
+++ comm_env.py
@@ -58,7 +58,14 @@ class CommunicationEnv(gym.Env):
 
     def step(self, sched_decision: np.array) -> None:
         """
-        sched_decisions is a matrix with dimensions BxNxM where B represents the number of basestations, N represents the maximum number of UEs and M the maximum number of RBs. For instance [[[1,1,0], [0,0,1]], [[0,0,1], [1,1,0]]] means that in the basestation 1, the UE 1 received the RBs 1 and 2 allocated while the second UE received the RB 3. For basestation 2, the UE 1 received the RB 3, and UE 2 got RBs 1 and 2. Remember that N and M value varies in according to the basestation configuration.
+        sched_decisions is a matrix with dimensions BxNxM where B represents
+        the number of basestations, N represents the maximum number of UEs
+        and M the maximum number of RBs. For instance
+        [[[1,1,0], [0,0,1]], [[0,0,1], [1,1,0]]] means that in the basestation
+        1, the UE 1 received the RBs 1 and 2 allocated while the second UE
+        received the RB 3. For basestation 2, the UE 1 received the RB 3, and
+        UE 2 got RBs 1 and 2. Remember that N and M value varies in according
+        to the basestation configuration.
         """
 
         mobilities = self.mobility.step()
