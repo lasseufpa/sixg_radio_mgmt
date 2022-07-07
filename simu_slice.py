5935090c3b8fb0597101ee0e42b3dc5536dc7c84
Cleverson Nahum <cleversonahum@gmail.com>
2022-07-07 17:20:47 -0300

    adding RR for RAN slicing scenario example

---
+++ simu_slice.py
@@ -0,0 +1,26 @@
+import numpy as np
+from tqdm import tqdm
+
+from agents.round_robin_slice import RoundRobin
+from channels.simple import SimpleChannel
+from comm_env import CommunicationEnv
+from mobilities.simple import SimpleMobility
+from traffics.simple import SimpleTraffic
+
+round_robin = RoundRobin(3, 2, [8, 8])
+comm_env = CommunicationEnv(
+    SimpleChannel,
+    SimpleTraffic,
+    SimpleMobility,
+    "simple_slice",
+    round_robin.action_format,
+    round_robin.obs_space_format,
+    round_robin.calculate_reward,
+)
+obs = comm_env.reset()
+number_steps = 10
+for step_number in tqdm(np.arange(comm_env.max_number_steps)):
+    sched_decision = round_robin.step(obs)
+    obs, _, end_ep, _ = comm_env.step(sched_decision)
+    if end_ep:
+        comm_env.reset()
