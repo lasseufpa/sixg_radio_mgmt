Considering instantaneous average latency and fixing association generation in the first step

sixg_radio_mgmt/buffer.py
@@ -14,8 +14,6 @@ class Buffer:
         Array representing the number of packets that waited in the buffer
         in each time step n, with maximum time step equals to the maximum
         packet latency allowed by max_packet_age variable
-    cumulative_buffer : np.ndarray
-        Auxiliary variable to calculate sent packets
     max_packets_buffer: int
         Maximum number of packets allowed in the buffer
     max_packets_age : int
@@ -47,7 +45,6 @@ class Buffer:
             Maximum latency that packets can wait in the buffer
         """
         self.buffer = np.zeros(max_packet_age + 1, dtype=int)
-        self.cumulative_buffer = np.zeros(max_packet_age + 1)
         self.max_packets_buffer = max_packets_buffer
         self.max_packets_age = max_packet_age
         self.dropped_packets = 0
@@ -104,7 +101,6 @@ class Buffer:
                 else:
                     self.buffer[i] -= packets_available_to_send
                     break
-        self.cumulative_buffer += np.subtract(tmp_buffer, self.buffer)
         self.sent_packets = np.sum(tmp_buffer) - np.sum(self.buffer)
 
     def get_buffer_occupancy(self) -> float:
@@ -125,13 +121,10 @@ class Buffer:
         float
             Average buffer delay
         """
-        if np.sum(self.cumulative_buffer) != 0:
+        if np.sum(self.buffer) != 0:
             return float(
-                np.sum(
-                    self.cumulative_buffer
-                    * np.arange(self.max_packets_age + 1)
-                )
-                / np.sum(self.cumulative_buffer)
+                np.sum(self.buffer * np.arange(self.max_packets_age + 1))
+                / np.sum(self.buffer)
             )
         else:
             return 0
@@ -153,8 +146,6 @@ def main():
         )
         ue_buffer.send_packets(sent_packets)
         print(ue_buffer.buffer, "\n")
-
-    print(ue_buffer.cumulative_buffer)
     print("UE Average delay: ", ue_buffer.get_avg_delay(), " ms")
 
 

sixg_radio_mgmt/comm_env.py
@@ -130,6 +130,8 @@ class CommunicationEnv(gym.Env):
         self.max_number_basestations = data["basestations"][
             "max_number_basestations"
         ]
+        self.max_number_slices = data["slices"]["max_number_slices"]
+        self.max_number_ues = data["ues"]["max_number_ues"]
         self.bandwidths = np.array(
             data["basestations"]["bandwidths"]
         )  # In MHz
@@ -156,22 +158,16 @@ class CommunicationEnv(gym.Env):
                 )
             )
         )
-
-        self.max_number_slices = data["slices"]["max_number_slices"]
         self.init_slice_ue_assoc = (
             np.array(data["slices"]["slice_ue_assoc"])
             if data["slices"].get("slice_ue_assoc") is not None
-            else np.ones(
-                (self.max_number_slices, data["slices"]["max_number_slices"])
-            )
+            else np.ones((self.max_number_slices, self.max_number_ues))
         )
         self.slice_req = (
             data["slices"]["slice_req"]
             if data["slices"].get("slice_req") is not None
             else {}
         )
-
-        self.max_number_ues = data["ues"]["max_number_ues"]
         self.max_buffer_latencies = (
             np.array(data["ues"]["max_buffer_latencies"])
             if data["ues"].get("max_buffer_latencies") is not None
@@ -319,21 +315,21 @@ class CommunicationEnv(gym.Env):
 
         if self.step_number == self.max_number_steps:
             self.metrics_hist.save(self.simu_name, self.episode_number)
-
-        # Update associations
-        (
-            self.basestations.ue_assoc,
-            self.basestations.slice_assoc,
-            self.slices.ue_assoc,
-            self.slices.requirements,
-        ) = self.associations.step(
-            self.basestations.ue_assoc,
-            self.basestations.slice_assoc,
-            self.slices.ue_assoc,
-            self.slices.requirements,
-            self.step_number,
-            self.episode_number,
-        )
+        else:
+            # Update associations
+            (
+                self.basestations.ue_assoc,
+                self.basestations.slice_assoc,
+                self.slices.ue_assoc,
+                self.slices.requirements,
+            ) = self.associations.step(
+                self.basestations.ue_assoc,
+                self.basestations.slice_assoc,
+                self.slices.ue_assoc,
+                self.slices.requirements,
+                self.step_number,
+                self.episode_number,
+            )
 
         return (
             obs,
@@ -544,6 +540,27 @@ class CommunicationEnv(gym.Env):
             self.max_buffer_pkts,
             self.pkt_sizes,
         )
+        self.associations = self.AssociationClass(
+            self.ues,
+            self.max_number_ues,
+            self.max_number_basestations,
+            self.max_number_slices,
+            self.rng,
+        )
+        # Update associations
+        (
+            self.init_basestation_ue_assoc,
+            self.init_basestation_slice_assoc,
+            self.init_slice_ue_assoc,
+            self.slice_req,
+        ) = self.associations.step(
+            self.init_basestation_ue_assoc,
+            self.init_basestation_slice_assoc,
+            self.init_slice_ue_assoc,
+            self.slice_req,
+            self.step_number,
+            self.episode_number,
+        )
         self.slices = Slices(
             self.max_number_slices,
             self.max_number_ues,
@@ -567,11 +584,4 @@ class CommunicationEnv(gym.Env):
             rng=self.rng,
         )
         self.traffic = self.TrafficClass(self.max_number_ues, rng=self.rng)
-        self.associations = self.AssociationClass(
-            self.ues,
-            self.max_number_ues,
-            self.max_number_basestations,
-            self.max_number_slices,
-            self.rng,
-        )
         self.metrics_hist = Metrics(self.hist_root_path)