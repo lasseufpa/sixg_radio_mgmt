689c4d72632d18926500f1184c034c82ba5dabf8
Cleverson Nahum <cleversonahum@gmail.com>
2024-04-01 17:14:44 -0400

    Fixing wrong location for condition to save hist

--- sixg_radio_mgmt/comm_env.py
+++ sixg_radio_mgmt/comm_env.py
@@ -332,10 +332,11 @@ class CommunicationEnv(gym.Env):
         step_hist.update({"reward": reward, "obs": obs})
         self.metrics_hist.step(step_hist)
 
-        if self.step_number == self.max_number_steps and self.save_hist:
-            self.metrics_hist.save(
-                self.simu_name, self.agent_name, self.episode_number
-            )
+        if self.step_number == self.max_number_steps:
+            if self.save_hist:
+                self.metrics_hist.save(
+                    self.simu_name, self.agent_name, self.episode_number
+                )
         else:
             # Update associations
             (
